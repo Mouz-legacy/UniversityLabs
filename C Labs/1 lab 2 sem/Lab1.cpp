745bb8cde5365e82a03f2423eb2d1859ab19f482
StreletsWorkA <59307803+StreletsWorkA@users.noreply.github.com>
2020-06-08 11:34:29 +0300

    fix Kurs + labs

---
+++ C Labs/1 lab 2 sem/Lab1.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a = 0, k = 3;
+	printf_s("Enter number = ");
+	scanf_s("%d", &a);
+
+	for (int l = 1;;l++)
+	{
+		k = (k << 2);
+
+		if (l == 2)
+		{
+			l = 0;
+			k += 3;
+		}
+
+		if (k > a) break;
+
+		for (int i = sizeof(k) * 8 - 1; i >= 0; --i)
+		{
+			printf_s("%d",k&(1<<i)?1:0);
+		}
+
+		printf("\n");
+		printf_s("%d \n", k);
+
+	}
+}
---
+++ C Labs/6 lab 2 sem/Lab6.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef char tSurname[20];
+typedef char tName[20];
+typedef int tKurs;
+typedef int tGroup;
+typedef int tViolations;
+typedef char tNumber[20];
+typedef int tRoom;
+typedef int tFloor;
+
+enum Faculty
+{
+    FKP = 1,
+    FITU,
+    FKSIS,
+    FRE,
+    FIK,
+    IEF,
+    VF
+};
+
+struct node
+{
+    tRoom room;
+    tFloor floor;
+    tSurname surname;
+    tName name;
+    tKurs kurs;
+    tGroup group;
+    tViolations violations;
+    Faculty faculty;
+    struct node* Next, * Prev;
+};
+
+node* root = NULL;
+
+struct node* Init()
+{
+    struct node* lst;
+    lst = (struct node*)malloc(sizeof(struct node));
+    return(lst);
+};
+
+int main()
+{
+    node my = *Init();
+    printf_s("enter");
+    scanf_s("%s", my.name);
+}
