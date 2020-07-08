e9dee9f254e68e1f10ccb11b2cc3518ae5593d23
StreletsWorkA <59307803+StreletsWorkA@users.noreply.github.com>
2020-07-08 21:54:23 +0300

    Bot

---
+++ C# Knowledge/Discord Bot/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+    }
+}
